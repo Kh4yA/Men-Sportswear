ec1f2f4d1a41e5e094ede9209abb4e0d57a56859
Kh4yA <87660873+Kh4yA@users.noreply.github.com>
2024-03-22 08:47:15 +0100

    verification

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 //Mini carte du footer 
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([50.433333, 2,833333], 9);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([50.433333, 2,833333]).addTo(map)
+    .bindPopup('Vous êtes ici.')
     .openPopup();
 
     //menu burger
